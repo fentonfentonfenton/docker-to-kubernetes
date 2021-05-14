dc2edf75b444d02241fcb337708f66e624b7d918
Jack Fenton <fenton@Jack-F-MM-C07DM063PJJ9.local>
2021-05-14 09:01:05 +0100

    docker compsoe and k8s files

---
+++ script.sh
@@ -0,0 +1,21 @@
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' > /usr/share/nginx/html/index.html
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo  '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
+
+echo '\n' >> /usr/share/nginx/html/index.html
+WORD=`curl -v --silent "https://random-word-api.herokuapp.com/word?number=1" 2>&1 | sed 's/\[\"//g' | sed 's/\"\]//g' | tail -c 5` ; curl -v --silent "https://news.google.com/rss/search?q=$WORD&hl=en-GB&gl=GB&ceid=GB:en"  2>&1 | awk -F'item' '{print $2}' | awk -F'item' '{print $1}' | head -c 150 | tail -c 80 | sed 's/title//g' | tr -cd '[a-zA-Z0-9 ]._-' >> /usr/share/nginx/html/index.html
